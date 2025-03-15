a44c3b59fe942f2ed54cc8d6dc4871b9b4e3f5ce
Burhanverse <burhanverse@gmail.com>
2025-03-15 17:03:11 +0530

    Don't allow shortening URLs from `droplinks` itself

--- ext/utils.mjs
+++ ext/utils.mjs
@@ -1,6 +1,12 @@
 export function isValidUrl(string) {
     try {
-        new URL(string);
+        const url = new URL(string);
+
+        // Don't allow shortening URLs from droplink.co itself
+        if (url.hostname === 'droplink.co' || url.hostname.endsWith('.droplink.co')) {
+            return false;
+        }
+
         return true;
     } catch (_) {
         return false;
